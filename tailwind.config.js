d9b71b8b8d41f85590a528755d9778c170c472cf
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-02 22:00:48 +0700

    feat color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       colors: {
         //convert rgba(44, 48, 54, 1) to hex
         primary: "#2c3036",
+        secondary: "#F0F0F0",
+        bgprimary: "#FFFFFF",
+        bgsecondary: "#D9D9D9",
       },
     },
   },
