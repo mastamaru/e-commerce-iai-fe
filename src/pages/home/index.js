9f253053dbd22aef24ef12840b57311ea0d3e6cc
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-04 03:53:56 +0700

    fix overflow

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -47,10 +47,10 @@ export default function HomeUser() {
 
   return (
     <>
-      <Navbar />
-      <section>
-        <div className="bg-[url('/herouser.png')] w-full h-[320px] bg-cover" />
-        <div className="flex flex-col items-center w-full min-h-[1400px] py-[120px] bg-gray-50 justify center">
+      <section className="overflow-x-hidden">
+        <Navbar />
+        <div className="bg-[url('/herouser.png')]  h-[320px] bg-cover overflow-x-hidden" />
+        <div className="flex flex-col items-center w-full min-h-[1400px] py-[120px] bg-gray-50 justify center overflow-x-hidden">
           <div className="container grid grid-cols-4 grid-rows-3 gap-x-14 gap-y-[90px] h-full items-center">
             {productList.map((product, index) => (
               <Product
