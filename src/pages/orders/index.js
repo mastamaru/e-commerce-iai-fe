ae33ec1ce30fe9ac430444c10db461a4ecfd8a62
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-04 04:59:48 +0700

    feat add orders page overflow

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -38,108 +38,109 @@ export default function Orders() {
   }, []);
 
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <Head>
-        <title>Order History</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className="bg-gray-100 min-h-screen overflow-x-hidden">
+        <Head>
+          <title>Order History</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <header className="bg-white shadow">
-        <Navbar />
-      </header>
+        <header className="bg-white shadow">
+          <Navbar />
+        </header>
 
-      <main className="container mx-auto px-4 py-8 h-[1200px]">
-        <h1 className="text-3xl font-bold mb-6">Orders History</h1>
-        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  Order ID
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  Date
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  Items
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  QTY
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  Order Total
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
-                  Status
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200 font-sarala text-primary ">
-              {orders.map((order, orderIndex) => (
-                <React.Fragment key={order._id}>
-                  {order.items.map((item, index) => (
-                    <tr
-                      key={index}
-                      className={`${
-                        orderIndex % 2 === 1 ? "bg-[#BCCCEB]" : ""
-                      } border-none`}
-                    >
-                      {index === 0 && (
-                        <td
-                          className="px-6 py-4 whitespace-nowrap text-[16.7px] leading-5"
-                          rowSpan={order.items.length}
-                        >
-                          {order.orderId}
-                        </td>
-                      )}
-                      {index === 0 && (
-                        <td
-                          className="px-6 py-4 whitespace-nowrap text-[16.7px] leading-5"
-                          rowSpan={order.items.length}
-                        >
-                          {order.date}
-                        </td>
-                      )}
-                      <td className="px-6 py-4 whitespace-nowrap  ">
-                        {item.name}
-                        <br />
-                        <span className="font-sarala font-bold ">
-                          Size: {item.size}
-                        </span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        {item.quantity}
-                      </td>
-                      {index === 0 && (
-                        <td
-                          className="px-6 py-4 whitespace-nowrap"
-                          rowSpan={order.items.length}
-                        >
-                          <span className="font-bold text-[16.7px] leading-[24px]">
-                            {order.total}
+        <main className="container mx-auto px-4 py-8 h-[100vh] overflow-y-scroll">
+          <h1 className="text-3xl font-bold mb-6">Orders History</h1>
+          <div className="bg-white shadow overflow-hidden sm:rounded-lg ">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    Order ID
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    Date
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    Items
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    QTY
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    Order Total
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 tracking-wider">
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200 font-sarala text-primary ">
+                {orders.map((order, orderIndex) => (
+                  <React.Fragment key={order._id}>
+                    {order.items.map((item, index) => (
+                      <tr
+                        key={index}
+                        className={`${
+                          orderIndex % 2 === 1 ? "bg-[#BCCCEB]" : ""
+                        } border-none`}
+                      >
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 whitespace-nowrap text-[16.7px] leading-5"
+                            rowSpan={order.items.length}
+                          >
+                            {order.orderId}
+                          </td>
+                        )}
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 whitespace-nowrap text-[16.7px] leading-5"
+                            rowSpan={order.items.length}
+                          >
+                            {order.date}
+                          </td>
+                        )}
+                        <td className="px-6 py-4 whitespace-nowrap  ">
+                          {item.name}
+                          <br />
+                          <span className="font-sarala font-bold ">
+                            Size: {item.size}
                           </span>
                         </td>
-                      )}
-                      {index === 0 && (
-                        <td
-                          className="px-6 py-4 whitespace-nowrap"
-                          rowSpan={order.items.length}
-                        >
-                          <button className="px-4 py-2 text-sm font-medium text-black border border-black bg-white-500">
-                            {order.status}
-                          </button>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.quantity}
                         </td>
-                      )}
-                    </tr>
-                  ))}
-                </React.Fragment>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 whitespace-nowrap"
+                            rowSpan={order.items.length}
+                          >
+                            <span className="font-bold text-[16.7px] leading-[24px]">
+                              {order.total}
+                            </span>
+                          </td>
+                        )}
+                        {index === 0 && (
+                          <td
+                            className="px-6 py-4 whitespace-nowrap"
+                            rowSpan={order.items.length}
+                          >
+                            <button className="px-4 py-2 text-sm font-medium text-black border border-black bg-white-500">
+                              {order.status}
+                            </button>
+                          </td>
+                        )}
+                      </tr>
+                    ))}
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
