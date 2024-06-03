fd7ad1a090a06682502cb31e48b45bd61853a37e
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-04 01:06:57 +0700

    update card product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import Image from "next/image";
-
-const available = true;
-const ready_s = available ? "/size-s-r.png" : "/size-s.png";
-const ready_m = available ? "/size-m-r.png" : "/size-m-s.png";
-const ready_l = available ? "/size-l-r.png" : "/size-l-s.png";
 
 export default function Product({
   name = "WGAMING Creativity Black Tee",
   price = "Rp 745.000",
   image = "/pererek-ts.png",
 }) {
+  const [selectedSize, setSelectedSize] = useState({
+    s: false,
+    m: false,
+    l: false,
+  });
+
+  const handleSizeClick = (size) => {
+    setSelectedSize((prev) => ({
+      ...prev,
+      [size]: !prev[size],
+    }));
+  };
+
+  const ready_s = selectedSize.s ? "/size-s-r.png" : "/size-s.png";
+  const ready_m = selectedSize.m ? "/size-m-r.png" : "/size-m-s.png";
+  const ready_l = selectedSize.l ? "/size-l-r.png" : "/size-l-s.png";
+
   return (
     <div className="flex flex-col items-center h-[288px]">
       <img src={image} alt="product" />
@@ -18,14 +31,29 @@ export default function Product({
         <h1>{name}</h1>
         <p>{price}</p>
       </div>
-      <div className="flex gap-3.5  w-[180px] items-center align-middle justify-center">
-        <img src="/size-s.png" about="size-s" className="text-primary" />
-        <img src="/size-m-r.png" about="size-s" />
-        <img src="/size-l-r.png" about="size-s" />
+      <div className="flex gap-3.5 w-[180px] items-center align-middle justify-center">
+        <img
+          src={ready_s}
+          alt="size-s"
+          className="cursor-pointer"
+          onClick={() => handleSizeClick("s")}
+        />
+        <img
+          src={ready_m}
+          alt="size-m"
+          className="cursor-pointer"
+          onClick={() => handleSizeClick("m")}
+        />
+        <img
+          src={ready_l}
+          alt="size-l"
+          className="cursor-pointer"
+          onClick={() => handleSizeClick("l")}
+        />
         <p className="text-primary font-semibold text-[16.67px] text-center">
           |
         </p>
-        <MdAddShoppingCart className="z-999 text-2xl" />
+        <MdAddShoppingCart className="z-999 text-2xl cursor-pointer" />
       </div>
     </div>
   );
