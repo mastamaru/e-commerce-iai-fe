01f40e8d5c44556db0e8adf1ca9158675814d233
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2024-06-04 05:32:53 +0700

    update index halal

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,20 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col items-center">
-        <h1 className="text-2xl text-center font-sarala ">RUUUDDYYYYYYYYY!</h1>
+      <div className="flex flex-col items-center pt-20">
+        <h1 className="text-4xl font-bold text-center font-sarala mb-4 ">
+          DON&apos;T FORGET TO BUY PAPER REX BUNDLE!
+        </h1>
         <div className="flex">
           <Image
-            src="/home2.jpg"
-            alt="Home"
-            width={800}
-            height={800}
-            className="mx-auto"
-          />
-          <Image
-            src="/home.png"
+            src="/pererekbundle.jpeg"
             alt="Home"
             width={800}
             height={800}
             className="mx-auto"
           />
         </div>
-        <InputText label="Email" type="email" />
-        <Navbar />
-        <Product />
       </div>
-      <Footer />
     </>
   );
 }
